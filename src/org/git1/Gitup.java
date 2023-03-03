211f3ff4459f2e4f25f7df65a0d7a96facd7285a
RajkumarV1489 <rajkumarv1489@gmail.com>
2023-03-03 11:13:41 +0530

    resolved issue

--- src/org/git1/Gitup.java
+++ src/org/git1/Gitup.java
@@ -1,19 +1,13 @@
 package org.git1;
 
 public class Gitup {
-	
+
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
-		System.out.println("Welcome");	
-		System.out.println("Welcome to B Branch");
-		
-=======
+
 		System.out.println("Welcome");
 		System.out.println("A Branch Credated");
->>>>>>> 5417288de4de8522ce2c63c355b98be7b2659977
-		
-		
+		System.out.println("Welcome to B Branch");
+
 	}
 
 }
